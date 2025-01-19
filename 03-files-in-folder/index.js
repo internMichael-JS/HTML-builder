26fe335f2d12107c64612bd08fb75074e29874cd
Michail <astapenko.m99@gmail.com>
2025-01-19 12:17:59 +0300

    fix: 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -11,9 +11,9 @@ fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
   files.forEach(async (file) => {
     if (!file.isDirectory()) {
       let exst = await path.extname(file.name.toString());
-      let filePath = path.join(folderPath, file.name.toString());
-      let fileName = path.basename(filePath);
-      fs.stat(filePath, async (err, stats) => {
+      let filePath = await path.join(folderPath, file.name.toString());
+      let fileName = await path.basename(filePath);
+      fs.stat(filePath, (err, stats) => {
         if (err) {
           console.error('Ошибка:', err);
           return;
